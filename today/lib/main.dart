a7581b91974df4d9b84d3eb687a7e3ba0a59a3dc
kjh3291 <128571125+kjh3291@users.noreply.github.com>
2023-11-14 11:03:58 +0900

    메인 화면을 생성 및 하단 바를 통하여 Dairy기능으로 이동 구현

--- today/lib/main.dart
+++ today/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:today/screens/diary_home/home_screens.dart';
 
 void main() {
-  initializeDateFormatting('ko_KR').then((_) { // 한국어 로케일 데이터 초기화
+  initializeDateFormatting('ko_KR').then((_) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
@@ -24,7 +24,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const DiaryScreen(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('상단'),
+      ),
+      body: Center(
+        child: const Text('중단'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: '다이어리',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: '캘린더',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '설정',
+          ),
+        ],
+        currentIndex: 1, // 두 번째 버튼 아이콘에 DiaryScreen 연결
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DiaryScreen(),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
