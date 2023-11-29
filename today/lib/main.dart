419e9dd457b73c698955d892f2deab3433110b4b
kjh3291 <128571125+kjh3291@users.noreply.github.com>
2023-11-29 22:38:23 +0900

    앱의 상단 부분의 UI 유사성을 위해 수정

--- today/lib/main.dart
+++ today/lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:today/screens/diary_home/home_screens.dart';
-
+import 'package:today/todolist/ToDoList.dart';
 import 'calendar/calendar_screen.dart';
+
 void main() {
   initializeDateFormatting('ko_KR').then((_) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -33,14 +34,36 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  Color getAppBarBackgroundColor() {
+    DateTime now = DateTime.now();
+    int dayOfWeek = now.weekday;
+
+    if (dayOfWeek == DateTime.monday) {
+      return Colors.yellow;
+    } else if (dayOfWeek == DateTime.tuesday) {
+      return Colors.pinkAccent;
+    } else if (dayOfWeek == DateTime.wednesday) {
+      return Colors.green;
+    } else if (dayOfWeek == DateTime.thursday) {
+      return Colors.orange;
+    } else if (dayOfWeek == DateTime.friday) {
+      return Colors.lightBlue;
+    } else if (dayOfWeek == DateTime.saturday) {
+      return Colors.orange;
+    } else {
+      return Colors.redAccent;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final now = DateTime.now();
-    final weekday = DateFormat('EEEE', 'ko_KR').format(now); // 요일
-    final formattedDate = DateFormat('yyyy년/MM/dd/($weekday)', 'ko_KR').format(now); // 년도, 날짜, 요일
+    final weekday = DateFormat('EEEE', 'ko_KR').format(now);
+    final formattedDate = DateFormat('yyyy년/MM/dd/($weekday)', 'ko_KR').format(now);
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: getAppBarBackgroundColor(),
         title: Row(
           children: [
             Text(formattedDate, style: TextStyle(color: Colors.black)),
@@ -53,23 +76,23 @@ class MyHomePage extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.black),
-            label: '홈',
+            icon: Icon(Icons.checklist, color: Colors.black),
+            label: '',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.book, color: Colors.black),
-            label: '다이어리',
+            label: '',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_today, color: Colors.black),
-            label: '캘린더',
+            label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings, color: Colors.black),
-            label: '설정',
+            icon: Icon(Icons.add_alarm_rounded, color: Colors.black),
+            label: '',
           ),
         ],
-        currentIndex: 1, // 두 번째 버튼 아이콘에 DiaryScreen 연결
+        currentIndex: 1,
         onTap: (index) {
           if (index == 1) {
             Navigator.push(
@@ -82,13 +105,20 @@ class MyHomePage extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) =>  CalendarScreen(),
+                builder: (context) => CalendarScreen(),
+              ),
+            );
+          } else if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ToDoList(),
               ),
             );
           }
         },
-        selectedItemColor: Colors.black, // 선택된 아이템의 색상
-        unselectedItemColor: Colors.black, // 선택되지 않은 아이템의 색상
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
       ),
     );
   }
