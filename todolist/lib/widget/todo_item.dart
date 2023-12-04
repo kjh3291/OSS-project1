26ae9513e0b734dcdc1bf92c22d9441f76c6259b
JongHyeok0706 <123792925+JongHyeok0706@users.noreply.github.com>
2023-12-04 22:33:37 +0900

    컨테이너 테두리 추가

--- todolist/lib/widget/todo_item.dart
+++ todolist/lib/widget/todo_item.dart
@@ -1,44 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:todolist/colors/color.dart';
 import 'package:todolist/model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
   final ToDo todo;
-  final onToDoChanged;
-  final onDeleteItem;
 
-  const ToDoItem({
-    Key? key,
-    required this.todo,
-    required this.onToDoChanged,
-    required this.onDeleteItem,
-  }) : super(key: key);
+  const ToDoItem({Key? key, required this.todo}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 20),
-      child: ListTile(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+        margin: EdgeInsets.only(bottom: 20),
+        child: ListTile(
         onTap: () {
-          // print('Clicked on Todo Item.');
-          onToDoChanged(todo);
+          print('Clicked on Todo Item.');
         },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
-        ),
+          ),
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        tileColor: Colors.white,
+          tileColor: Colors.white,
         leading: Icon(
           todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
-          color: tdBlue,
+          color: Colors.blue,
         ),
         title: Text(
           todo.todoText!,
           style: TextStyle(
-            fontSize: 16,
-            color: tdBlack,
-            decoration: todo.isDone ? TextDecoration.lineThrough : null,
-          ),
+              fontSize: 16,
+              color:Colors.black,
+              decoration: todo.isDone? TextDecoration.lineThrough : null,
+        ),
         ),
         trailing: Container(
           padding: EdgeInsets.all(0),
@@ -46,7 +43,7 @@ class ToDoItem extends StatelessWidget {
           height: 35,
           width: 35,
           decoration: BoxDecoration(
-            color: tdRed,
+            color: Colors.red,
             borderRadius: BorderRadius.circular(5),
           ),
           child: IconButton(
@@ -54,8 +51,7 @@ class ToDoItem extends StatelessWidget {
             iconSize: 18,
             icon: Icon(Icons.delete),
             onPressed: () {
-              // print('Clicked on delete icon');
-              onDeleteItem(todo.id);
+              print('Clicked on delete icon.');
             },
           ),
         ),
