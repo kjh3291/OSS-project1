98984f1461e26275f48783574242106023cc81fa
JongHyeok0706 <123792925+JongHyeok0706@users.noreply.github.com>
2023-12-04 23:39:42 +0900

    뒤로 돌아가는 기능을 만들기 위한 코드 작성

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -258,3 +258,17 @@ class _ToDoListState extends State<ToDoList> {
     }
   }
 }
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
